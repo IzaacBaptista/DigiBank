ef551cd3ec856dd841c4111ad3abd567f708fa01
Izaac Baptista <izaacbaptista@gmail.com>
2021-02-21 12:45:13 -0300

    teste

---
+++ Classes/Layout.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigiBank.Classes
+{
+    public class Layout
+    {
+        public static void TelaPrincipal()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.White;
+        }
+    }
+
+}
